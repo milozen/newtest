0fcd4136633a6c2504887a1fc4f5896a47e6960e
milozen <50772707+milozen@users.noreply.github.com>
2023-06-15 10:54:07 +0800

    Update login.java

--- login.java
+++ login.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -120,6 +120,11 @@ public class Login extends BaseActivity {
 
     @OnClick(R.id.login_wx_image)
     public void wxClick(){
+        if (!checkBox.isChecked()) {
+            // 如果用户没有勾选隐私协议，显示一个Toast提示，并返回
+            Toast.makeText(Login.this,"请阅读《用户协议》和《隐私政策》后勾选同意",Toast.LENGTH_SHORT).show();
+            return;
+        }
         UMShareAPI.get(this).getPlatformInfo(Login.this,SHARE_MEDIA.WEIXIN,umAuthListener);
     }
 
